e0f6ead8dc5c3276301ca3b8906172a42aaae6d3
Alejandro DevOps <71050752+achdevops@users.noreply.github.com>
2021-04-20 09:35:14 -0300

    Update nv-deployment.tf

--- nv-deployment.tf
+++ nv-deployment.tf
@@ -1,20 +1,35 @@
+# pass your sensitive data with tfvars
+variable "sensitive_vars" {
+  type = object({
+    docker_user    = string
+    docker_pass    = string
+    admin_pass     = string
+  })
+  sensitive = true
+}
+
 module "nv-deployment" {
-source                  = "git::https://github.com/devseclabs/nv-deployment/?ref=main"
+    source                  = "git::https://github.com/devseclabs/nv-deployment.git?ref=main"
 
-# dockerhub by default
-registry_username   = "registry-user"
-registry_password   = "registry-pass"
-#registry_server     = "registry-server"
+    #see provider.tf - k8s context configured in ~/-kube/config
+    context                 =   "nvdemo-context"
 
-#provider - k8s context
-context = "my-k8s-context"
+    # dockerhub by default
+    registry_username   = var.sensitive_vars.docker_user
+    registry_password   = var.sensitive_vars.docker_pass
+    #registry_server     = "registry-server"
 
-#nv conf
-manager_svc_type = "LoadBalancer"
-scanner_replicas = 1
-controller_replicas = 3
-nv_version = "4.2.0"
+    #config enable
+    enable_config = true
+    #change admin pass
+    pass = var.sensitive_vars.admin_pass
 
-#license data
-license = file(".secret/license.txt")
+    #nv conf
+    manager_svc_type = "LoadBalancer"
+    scanner_replicas = 1
+    controller_replicas = 3
+    nv_version = "4.2.1"
+
+    #license data
+    license = ".secret/license.txt"
 }
